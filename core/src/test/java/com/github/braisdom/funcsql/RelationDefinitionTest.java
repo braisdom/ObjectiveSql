c0cc4d8bc88c3178d55ea6ac79613d95760a1a9f
Braisdom <yonghe.wang@joowing.com>
2020-07-16 14:12:06 +0800

    Add relation unit test

--- core/src/test/java/com/github/braisdom/funcsql/RelationDefinitionTest.java
+++ core/src/test/java/com/github/braisdom/funcsql/RelationDefinitionTest.java
@@ -14,17 +14,49 @@ public class RelationDefinitionTest {
 
         @Relation(relationType = RelationType.HAS_MANY)
         private List<TestRelativeModel> testRelativeModels;
+
+        @Relation(relationType = RelationType.HAS_MANY, primaryKey = "test_id", foreignKey = "given_foreign_id")
+        private List<TestRelativeModel> testRelativeModels2;
     }
 
     @DomainModel
     public static class TestRelativeModel {
-
+        @Relation(relationType = RelationType.BELONGS_TO)
+        private TestDomainModel testDomainModel;
     }
 
     @Test
-    public void testPrimaryKey() {
+    public void testDefaultPrimaryKey() {
         RelationDefinition relationDefinition = RelationDefinition
                 .createRelation(TestDomainModel.class, "testRelativeModels");
         Assertions.assertEquals(relationDefinition.getPrimaryKey(), "id");
+    }
+
+    @Test
+    public void testGivenPrimaryKey() {
+        RelationDefinition relationDefinition = RelationDefinition
+                .createRelation(TestDomainModel.class, "testRelativeModels2");
+        Assertions.assertEquals(relationDefinition.getPrimaryKey(), "test_id");
+    }
+
+    @Test
+    public void testHasAnyDefaultForeignKey() {
+        RelationDefinition relationDefinition = RelationDefinition
+                .createRelation(TestDomainModel.class, "testRelativeModels2");
+        Assertions.assertEquals(relationDefinition.getForeignKey(), "test_domain_model_id");
+    }
+
+    @Test
+    public void testGivenForeignKey() {
+        RelationDefinition relationDefinition = RelationDefinition
+                .createRelation(TestDomainModel.class, "testRelativeModels2");
+        Assertions.assertEquals(relationDefinition.getForeignKey(), "given_foreign_id");
+    }
+
+    @Test
+    public void testBelongsToDefaultForeignKey() {
+        RelationDefinition relationDefinition = RelationDefinition
+                .createRelation(TestRelativeModel.class, "testDomainModel");
+        Assertions.assertEquals(relationDefinition.getForeignKey(), "test_domain_model_id");
     }
 }
