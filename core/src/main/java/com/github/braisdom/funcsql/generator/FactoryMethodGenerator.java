a6f2fbde5955f60b19a59f11a456e64857208a1a
Braisdom <yonghe.wang@joowing.com>
2020-07-30 11:42:19 +0800

    Refactoring model descriptor

--- core/src/main/java/com/github/braisdom/funcsql/generator/FactoryMethodGenerator.java
+++ core/src/main/java/com/github/braisdom/funcsql/generator/FactoryMethodGenerator.java
@@ -1,18 +1,81 @@
 package com.github.braisdom.funcsql.generator;
 
+import com.github.braisdom.funcsql.Database;
+import com.github.braisdom.funcsql.Persistence;
+import com.github.braisdom.funcsql.PersistenceFactory;
 import com.github.braisdom.funcsql.annotations.DomainModel;
+import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.*;
+import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.ListBuffer;
 import lombok.core.AnnotationValues;
 import lombok.javac.JavacAnnotationHandler;
 import lombok.javac.JavacNode;
+import lombok.javac.JavacTreeMaker;
 import org.mangosdk.spi.ProviderFor;
+
+import static com.github.braisdom.funcsql.util.StringUtil.splitNameOf;
+import static lombok.javac.handlers.JavacHandlerUtil.chainDots;
+import static lombok.javac.handlers.JavacHandlerUtil.injectMethod;
 
 @ProviderFor(JavacAnnotationHandler.class)
 public class FactoryMethodGenerator extends JavacAnnotationHandler<DomainModel> {
 
     @Override
     public void handle(AnnotationValues<DomainModel> annotation, JCTree.JCAnnotation ast, JavacNode annotationNode) {
+        JavacTreeMaker treeMaker = annotationNode.getTreeMaker();
+        JavacNode typeNode = annotationNode.up();
 
+        JCMethodDecl createPersistenceMethod = createPersistenceMethod(treeMaker, typeNode);
+
+        injectMethod(typeNode, createPersistenceMethod);
     }
 
+    private JCMethodDecl createPersistenceMethod(JavacTreeMaker treeMaker, JavacNode typeNode) {
+        ListBuffer<JCStatement> jcStatements = new ListBuffer<>();
+
+        jcStatements.append(
+                treeMaker.VarDef(
+                        treeMaker.Modifiers(Flags.PARAMETER),
+                        typeNode.toName("persistenceFactory"),
+                        chainDots(typeNode, splitNameOf(PersistenceFactory.class)),
+                        treeMaker.Apply(
+                                List.nil(),
+                                treeMaker.Select(
+                                        chainDots(typeNode, splitNameOf(Database.class)),
+                                        typeNode.toName("getPersistenceFactory")
+                                ), List.nil()
+                        )
+                )
+        );
+
+        jcStatements.append(
+                treeMaker.Return(
+                        treeMaker.Apply(
+                                List.nil(),
+                                treeMaker.Select(
+                                        treeMaker.Ident(typeNode.toName("persistenceFactory")),
+                                        typeNode.toName("persistenceFactory")
+                                )
+                                , List.of(
+                                        treeMaker.Select(
+                                                treeMaker.Ident(typeNode.toName(typeNode.getName())),
+                                                typeNode.toName("class")
+                                        )
+                                )
+                        ))
+        );
+
+        return treeMaker.MethodDef(
+                treeMaker.Modifiers(Flags.PUBLIC + Flags.STATIC + Flags.FINAL),
+                typeNode.toName("createPersistence"),
+                chainDots(typeNode, splitNameOf(Persistence.class)),
+                List.nil(),
+                List.nil(),
+                List.nil(),
+                treeMaker.Block(0, jcStatements.toList()),
+                null
+        );
+    }
 }
--- core/src/main/java/com/github/braisdom/funcsql/generator/PrimaryFieldGenerator.java
+++ core/src/main/java/com/github/braisdom/funcsql/generator/PrimaryFieldGenerator.java
@@ -1,10 +1,14 @@
 package com.github.braisdom.funcsql.generator;
 
+import com.github.braisdom.funcsql.Database;
+import com.github.braisdom.funcsql.Persistence;
+import com.github.braisdom.funcsql.PersistenceFactory;
 import com.github.braisdom.funcsql.annotations.DomainModel;
 import com.github.braisdom.funcsql.annotations.PrimaryKey;
 import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.ListBuffer;
 import lombok.AccessLevel;
 import lombok.core.AST;
 import lombok.core.AnnotationValues;
@@ -17,7 +21,7 @@ import lombok.javac.handlers.JavacHandlerUtil;
 import org.mangosdk.spi.ProviderFor;
 
 import static com.github.braisdom.funcsql.util.StringUtil.splitNameOf;
-import static lombok.javac.handlers.JavacHandlerUtil.chainDots;
+import static lombok.javac.handlers.JavacHandlerUtil.*;
 
 @ProviderFor(JavacAnnotationHandler.class)
 public class PrimaryFieldGenerator extends JavacAnnotationHandler<DomainModel> {
@@ -50,6 +54,56 @@ public class PrimaryFieldGenerator extends JavacAnnotationHandler<DomainModel> {
         new HandleGetter().generateGetterForField(fieldNode,null, AccessLevel.PUBLIC, false);
         new HandleSetter().generateSetterForField(fieldNode, typeNode, AccessLevel.PUBLIC);
 
-        JavacHandlerUtil.injectField(typeNode, fieldDecl);
+        JCTree.JCMethodDecl createPersistenceMethod = createPersistenceMethod(treeMaker, typeNode);
+
+        injectMethod(typeNode, createPersistenceMethod);
+        injectField(typeNode, fieldDecl);
+    }
+
+    private JCTree.JCMethodDecl createPersistenceMethod(JavacTreeMaker treeMaker, JavacNode typeNode) {
+        ListBuffer<JCTree.JCStatement> jcStatements = new ListBuffer<>();
+
+        jcStatements.append(
+                treeMaker.VarDef(
+                        treeMaker.Modifiers(Flags.PARAMETER),
+                        typeNode.toName("persistenceFactory"),
+                        chainDots(typeNode, splitNameOf(PersistenceFactory.class)),
+                        treeMaker.Apply(
+                                List.nil(),
+                                treeMaker.Select(
+                                        chainDots(typeNode, splitNameOf(Database.class)),
+                                        typeNode.toName("getPersistenceFactory")
+                                ), List.nil()
+                        )
+                )
+        );
+
+        jcStatements.append(
+                treeMaker.Return(
+                        treeMaker.Apply(
+                                List.nil(),
+                                treeMaker.Select(
+                                        treeMaker.Ident(typeNode.toName("persistenceFactory")),
+                                        typeNode.toName("createPersistence")
+                                )
+                                , List.of(
+                                        treeMaker.Select(
+                                                treeMaker.Ident(typeNode.toName(typeNode.getName())),
+                                                typeNode.toName("class")
+                                        )
+                                )
+                        ))
+        );
+
+        return treeMaker.MethodDef(
+                treeMaker.Modifiers(Flags.PUBLIC + Flags.STATIC + Flags.FINAL),
+                typeNode.toName("createPersistence"),
+                chainDots(typeNode, splitNameOf(Persistence.class)),
+                List.nil(),
+                List.nil(),
+                List.nil(),
+                treeMaker.Block(0, jcStatements.toList()),
+                null
+        );
     }
 }
