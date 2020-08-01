b9bd1ce0b94a429e2ce9554303e6830b245f3000
Braisdom <yonghe.wang@joowing.com>
2020-08-01 22:53:53 +0800

    Add logger support

--- core/src/main/java/com/github/braisdom/funcsql/Logger.java
+++ core/src/main/java/com/github/braisdom/funcsql/Logger.java
@@ -1,4 +1,10 @@
 package com.github.braisdom.funcsql;
 
 public interface Logger {
+
+    boolean isDebugEnabled();
+
+    void debug(long elapsed, String sql, Object[] params);
+
+    void info(long elapsed, String sql, Object[] params);
 }
