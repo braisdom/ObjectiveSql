4a3f13682775d8a5dfb157d76c6308d054d6eedf
Braisdom <yonghe.wang@joowing.com>
2020-08-31 15:22:27 +0800

    Add OO sql

--- core/src/main/java/com/github/braisdom/funcsql/sql/expression/DefaultExpression.java
+++ core/src/main/java/com/github/braisdom/funcsql/sql/expression/DefaultExpression.java
@@ -3,17 +3,12 @@ package com.github.braisdom.funcsql.sql.expression;
 import com.github.braisdom.funcsql.sql.Expression;
 import com.github.braisdom.funcsql.sql.SQLContext;
 
-public class DefaultExpression implements Expression {
+public class DefaultExpression extends AbstractExpression {
 
     private final Object expression;
 
     public DefaultExpression(Object expression) {
         this.expression = expression;
-    }
-
-    @Override
-    public Expression as(String alias) {
-        return this;
     }
 
     @Override
--- core/src/main/java/com/github/braisdom/funcsql/sql/expression/Literal.java
+++ core/src/main/java/com/github/braisdom/funcsql/sql/expression/Literal.java
@@ -14,6 +14,8 @@ public class Literal extends AbstractExpression {
     public String toSql(SQLContext sqlContext) {
         if(rawLiteral == null)
             return "NULL";
+        if(String.class.isAssignableFrom(rawLiteral.getClass()))
+            return String.format("'%s'", String.valueOf(rawLiteral));
         return String.valueOf(rawLiteral);
     }
 }
