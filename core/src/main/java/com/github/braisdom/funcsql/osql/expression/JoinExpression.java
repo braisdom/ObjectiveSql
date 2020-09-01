e122c1cd5dcf718adb5548b44563eab12693071a
Braisdom <yonghe.wang@joowing.com>
2020-09-01 13:30:54 +0800

    Add OO sql

--- core/src/main/java/com/github/braisdom/funcsql/osql/expression/JoinExpression.java
+++ core/src/main/java/com/github/braisdom/funcsql/osql/expression/JoinExpression.java
@@ -2,8 +2,9 @@ package com.github.braisdom.funcsql.osql.expression;
 
 import com.github.braisdom.funcsql.osql.Dataset;
 import com.github.braisdom.funcsql.osql.Expression;
+import com.github.braisdom.funcsql.osql.ExpressionContext;
 
-public class JoinExpression {
+public class JoinExpression implements Expression {
     public static final int LEFT_OUTER_JOIN = 1;
     public static final int RIGHT_OUTER_JOIN = 2;
     public static final int INNER_JOIN = 3;
@@ -17,5 +18,15 @@ public class JoinExpression {
         this.joinType = joinType;
         this.dataset = dataset;
         this.onExpression = onExpression;
+    }
+
+    @Override
+    public Expression as(String alias) {
+        throw new UnsupportedOperationException("The join expression cannot be aliased");
+    }
+
+    @Override
+    public String toSql(ExpressionContext expressionContext) {
+        return null;
     }
 }
