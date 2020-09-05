b9f71854d9126615834acca567f00a9a5dee024b
Braisdom <yonghe.wang@joowing.com>
2020-09-05 21:25:03 +0800

    Add transaction support

--- core/src/main/java/com/github/braisdom/objsql/sql/Delete.java
+++
@@ -1,5 +0,0 @@
-package com.github.braisdom.objsql.sql;
-
-public class Delete {
-    // TODO It is not necessary now
-}
--- core/src/main/java/com/github/braisdom/objsql/sql/TemporaryTable.java
+++
@@ -1,9 +0,0 @@
-package com.github.braisdom.objsql.sql;
-
-public class TemporaryTable extends AbstractExpression implements Dataset {
-
-    @Override
-    public String toSql(ExpressionContext expressionContext) {
-        return null;
-    }
-}
