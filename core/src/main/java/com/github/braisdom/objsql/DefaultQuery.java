bf4fdc528ca0f5661783ede7c7201ec3896f133f
Braisdom <yonghe.wang@joowing.com>
2020-09-08 11:28:31 +0800

    Add comments

--- core/src/main/java/com/github/braisdom/objsql/DefaultQuery.java
+++ core/src/main/java/com/github/braisdom/objsql/DefaultQuery.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.braisdom.objsql;
 
 import com.github.braisdom.objsql.relation.Relationship;
@@ -9,6 +25,10 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * The default implementation of <code>Query</code> with JavaBean
+ * @param <T>
+ */
 public class DefaultQuery<T> extends AbstractQuery<T> {
 
     private static final String SELECT_STATEMENT = "SELECT %s FROM %s";
