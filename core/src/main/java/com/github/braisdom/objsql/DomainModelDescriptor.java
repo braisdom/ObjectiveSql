ceab98b34afbdd68459f4e4fc047cfb08c9bb5ff
Braisdom <yonghe.wang@joowing.com>
2020-09-08 13:31:04 +0800

    Add comments

--- core/src/main/java/com/github/braisdom/objsql/DomainModelDescriptor.java
+++ core/src/main/java/com/github/braisdom/objsql/DomainModelDescriptor.java
@@ -1,8 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.braisdom.objsql;
 
 import com.github.braisdom.objsql.annotations.PrimaryKey;
 import com.github.braisdom.objsql.transition.ColumnTransitional;
 
+/**
+ * It describes that a <code>DomainModel</code> can be adapted to ObjectiveSql。
+ * The main purpose of the abstraction is that let the non JavaBean can be saved and queried.
+ * For example, a object from ProtoBuffer can be saved into database immediately,
+ * no conversion to Javabeans is required
+ *
+ * @param <T>
+ */
 public interface DomainModelDescriptor<T> {
 
     T newInstance();
--- core/src/main/java/com/github/braisdom/objsql/QueryFactory.java
+++ core/src/main/java/com/github/braisdom/objsql/QueryFactory.java
@@ -1,5 +1,10 @@
 package com.github.braisdom.objsql;
 
+/**
+ * A abstract factory for creating the Query
+ *
+ * @see Query
+ */
 public interface QueryFactory {
 
     <T> Query<T> createQuery(Class<T> clazz);
