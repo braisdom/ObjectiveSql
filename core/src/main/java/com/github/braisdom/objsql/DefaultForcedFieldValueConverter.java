a5a0e8cb330220935e794dcf45e57f8350bdaf4f
Braisdom <yonghe.wang@joowing.com>
2020-09-18 10:04:07 +0800

    Add field value converter

--- core/src/main/java/com/github/braisdom/objsql/DefaultForcedFieldValueConverter.java
+++ core/src/main/java/com/github/braisdom/objsql/DefaultForcedFieldValueConverter.java
@@ -4,75 +4,81 @@ import java.math.BigInteger;
 import java.sql.Timestamp;
 
 public class DefaultForcedFieldValueConverter implements ForcedFieldValueConverter {
-    
+
     @Override
-    public Float risingFloat(Object raw) {
-        if(raw instanceof Float)
+    public Float toFloat(Object raw) {
+        if (raw instanceof Float)
             return (Float) raw;
-        else if(raw instanceof Double)
+        else if (raw instanceof Double)
             return Float.valueOf(String.valueOf(raw));
-        else if(raw instanceof Integer)
+        else if (raw instanceof Integer)
             return Float.valueOf(String.valueOf(raw));
+        else if(raw instanceof String)
+            return Float.valueOf((String) raw);
         else
             throw new IllegalArgumentException(String.format("'%s' cannot convert to Float", String.valueOf(raw)));
     }
 
     @Override
-    public Double risingDouble(Object raw) {
-        if(raw instanceof Double)
+    public Double toDouble(Object raw) {
+        if (raw instanceof Double)
             return (Double) raw;
-        else if(raw instanceof Float)
+        else if (raw instanceof Float)
             return Double.valueOf(String.valueOf(raw));
-        else if(raw instanceof Integer)
+        else if (raw instanceof Integer)
             return Double.valueOf(String.valueOf(raw));
+        else if (raw instanceof String)
+            return Double.valueOf((String) raw);
         else
             throw new IllegalArgumentException(String.format("'%s' cannot convert to Double", String.valueOf(raw)));
     }
 
     @Override
-    public Short risingShort(Object raw) {
-        if(raw instanceof Short)
+    public Short toShort(Object raw) {
+        if (raw instanceof Short)
             return (Short) raw;
-        else if(raw instanceof Integer)
+        else if (raw instanceof Integer)
             return Short.valueOf(String.valueOf(raw));
         else
             throw new IllegalArgumentException(String.format("'%s' cannot convert to Short", String.valueOf(raw)));
     }
 
     @Override
-    public Integer risingInteger(Object raw) {
-        if(raw instanceof Integer)
+    public Integer toInteger(Object raw) {
+        if (raw instanceof Integer)
             return (Integer) raw;
-        else if(raw instanceof Long)
+        else if (raw instanceof Long)
             return Integer.valueOf(String.valueOf(raw));
-        else if(raw instanceof BigInteger)
-            return ((BigInteger)raw).intValue();
+        else if (raw instanceof BigInteger)
+            return ((BigInteger) raw).intValue();
+        else if (raw instanceof String)
+            return Integer.valueOf((String) raw);
         else
             throw new IllegalArgumentException(String.format("'%s' cannot convert to Integer", String.valueOf(raw)));
     }
 
     @Override
-    public Long risingLong(Object raw) {
-        if(raw instanceof Long)
+    public Long toLong(Object raw) {
+        if (raw instanceof Long)
             return (Long) raw;
-        else if(raw instanceof Integer)
+        else if (raw instanceof Integer)
             return Long.valueOf(String.valueOf(raw));
         else
             throw new IllegalArgumentException(String.format("'%s' cannot convert to Long", String.valueOf(raw)));
     }
 
     @Override
-    public Boolean risingBoolean(Object raw) {
-        if(raw instanceof Integer)
-            return ((Integer)raw) == 1;
-        if(raw instanceof Short)
-            return ((Short)raw) == 1;
+    public Boolean toBoolean(Object raw) {
+        if (raw instanceof Integer)
+            return ((Integer) raw) == 1;
+        if (raw instanceof Short)
+            return ((Short) raw) == 1;
         else
             throw new IllegalArgumentException(String.format("'%s' cannot convert to Boolean", String.valueOf(raw)));
     }
 
     @Override
-    public Timestamp risingTimestamp(Object raw) {
+    public Timestamp toTimestamp(Object raw) {
         return null;
     }
 }
