4d3447e123c354ef5c36f9bd462a38c1724fbb49
Braisdom <yonghe.wang@joowing.com>
2020-09-19 22:47:52 +0800

    Refactoring property read and write

---
+++ springboot/src/main/java/com/github/braisdom/example/model/Product.java
@@ -0,0 +1,14 @@
+package com.github.braisdom.example.model;
+
+import com.github.braisdom.objsql.annotations.DomainModel;
+import com.github.braisdom.objsql.annotations.Queryable;
+
+@DomainModel
+public class Product {
+    @Queryable
+    private String barcode;
+    private String name;
+    private Integer categoryId;
+    private Double salesPrice;
+    private Double cost;
+}
--- springboot/src/main/resources/mysql.sql
+++ springboot/src/main/resources/mysql.sql
@@ -33,3 +33,14 @@ CREATE TABLE IF NOT EXISTS `objective_sql`.`order_lines`(
    `quantity` FLOAT,
    PRIMARY KEY ( `id` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `objective_sql`.`products`(
+   `id` INT UNSIGNED AUTO_INCREMENT,
+   `barcode` VARCHAR(100),
+   `name` INT(10),
+   `category_id` VARCHAR(100),
+   `sales_price` FLOAT,
+   `cost` FLOAT,
+   PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
