4e54fcdeda2e88d584261db724a1f065f9d9b0e8
Braisdom <yonghe.wang@joowing.com>
2020-09-25 11:13:53 +0800

    Optimizing expressions

--- core/src/main/java/com/github/braisdom/objsql/ConnectionFactory.java
+++ core/src/main/java/com/github/braisdom/objsql/ConnectionFactory.java
@@ -21,9 +21,9 @@ import java.sql.SQLException;
 
 /**
  * A factory for creating the <code>Connection</code> of database, it is necessary
- * for ObjectiveSql in runtime. It will be used in <code>Databases.installConnectionFactory</code> method,
- * for a application customizes various connections.<br/>
- * <b>Notice:</b> The ConnectionFactory will be inject at the application beginning.
+ * for ObjectiveSql in runtime.<br/>
+ *
+ * <b>Notice:</b> The ConnectionFactory must be injected at the application beginning.
  *
  * @see Databases#installConnectionFactory(ConnectionFactory)
  */
