fdb19c709c2261f7c05161f3897443943eb54bd0
Braisdom <yonghe.wang@joowing.com>
2020-09-30 17:04:52 +0800

    Optimizing expressions

--- core/src/main/java/com/github/braisdom/objsql/ForcedFieldValueConverter.java
+++ core/src/main/java/com/github/braisdom/objsql/ForcedFieldValueConverter.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 /**
  * The class is used for converting the field value from various raw value.
  * Such as, a string into float, a integer into float, etc. It is an utility tools
- * for converting JSON to Java Bean or column value to Java Bean.
+ * for converting JSON to Java Bean.
  * For example:<br/>
  * <pre>
  *     ForcedFieldValueConverter convert = new DefaultForcedFieldValueConverter();
