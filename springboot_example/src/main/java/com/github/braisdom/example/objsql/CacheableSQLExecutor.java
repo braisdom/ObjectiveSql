b9ca4313cddd3b076ada49d5342b13a5e157c175
Braisdom <yonghe.wang@joowing.com>
2020-10-13 17:48:02 +0800

    Add redis support

--- springboot_example/src/main/java/com/github/braisdom/example/objsql/CacheableSQLExecutor.java
+++ springboot_example/src/main/java/com/github/braisdom/example/objsql/CacheableSQLExecutor.java
@@ -5,7 +5,9 @@ import com.github.braisdom.objsql.DefaultSQLExecutor;
 import com.github.braisdom.objsql.TableRowAdapter;
 import org.springframework.util.SerializationUtils;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -16,7 +18,9 @@ import java.util.List;
 
 public class CacheableSQLExecutor<T> extends DefaultSQLExecutor<T> {
 
-    private static final List<Class<?>> CACHEABLE_CLASSES = Arrays.asList(new Class[]{Member.class});
+    private static final List<Class<? extends Serializable>> CACHEABLE_CLASSES =
+            Arrays.asList(new Class[]{Member.class});
+    private static final Integer CACHED_OBJECT_EXPIRED = 60;
     private static final String KEY_SHA = "SHA";
 
     private Jedis jedis = new Jedis("127.0.0.1", 6379);
@@ -33,16 +37,25 @@ public class CacheableSQLExecutor<T> extends DefaultSQLExecutor<T> {
     @Override
     public List<T> query(Connection connection, String sql,
                          TableRowAdapter tableRowAdapter, Object... params) throws SQLException {
-        if (CACHEABLE_CLASSES.contains(tableRowAdapter.getDomainModelClass())) {
+        Class<?> domainClass = tableRowAdapter.getDomainModelClass();
+
+        if (CACHEABLE_CLASSES.contains(domainClass)) {
+            if(!Serializable.class.isAssignableFrom(domainClass))
+                throw new IllegalArgumentException(String.format("The %s cannot be serialized"));
+
             messageDigest.update(sql.getBytes());
+
             String hashedSqlId = new BigInteger(messageDigest.digest()).toString(64);
             byte[] rawObjects = jedis.get(hashedSqlId.getBytes());
+
             if (rawObjects != null) {
                 return (List<T>) SerializationUtils.deserialize(rawObjects);
             } else {
                 List<T> objects = super.query(connection, sql, tableRowAdapter, params);
                 byte[] encodedObjects = SerializationUtils.serialize(objects);
-                jedis.set(hashedSqlId.getBytes(), encodedObjects);
+                SetParams expiredParams = SetParams.setParams().ex(CACHED_OBJECT_EXPIRED);
+
+                jedis.set(hashedSqlId.getBytes(), encodedObjects, expiredParams);
 
                 return objects;
             }
