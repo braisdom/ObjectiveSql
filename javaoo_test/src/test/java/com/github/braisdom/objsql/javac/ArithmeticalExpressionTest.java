d8c8858f97900b50bdb541f45754f34ec69f9935
Braisdom <yonghe.wang@joowing.com>
2020-11-12 22:57:55 +0800

    Add java operator overloading test

--- javaoo_test/src/test/java/com/github/braisdom/objsql/javac/ArithmeticalExpressionTest.java
+++ javaoo_test/src/test/java/com/github/braisdom/objsql/javac/ArithmeticalExpressionTest.java
@@ -28,8 +28,13 @@ public class ArithmeticalExpressionTest {
         long integerPlus2 = 1 + 1L;
         int integerPlus3 = 1 + (short)1;
         int integerPlus4 = 1 + (byte)1;
-        float integerPlus5 = 1 + (float)1.2;
-        double integerPlus6 = 1 + 1.2d;
+        Float integerPlus5 = 1 + 1.2f;
+        Double integerPlus6 = 1d + 1.2d;
+        Float integerPlus7 = 1.2f + 1;
+        Double integerPlus8 = 1.2d + 1;
+        long integerPlus9 = 1L + 1;
+        int integerPlus10 = (short)1 + 1;
+        int integerPlus11 = (byte)1 + 1;
 
         Assertions.assertEquals(numberPlus.toSql(mysql), "((1 + 1))");
         Assertions.assertEquals(numberPlus2.toSql(mysql), "((1 + ((1 + 1))))");
@@ -44,7 +49,150 @@ public class ArithmeticalExpressionTest {
         Assertions.assertEquals(integerPlus2, 2);
         Assertions.assertEquals(integerPlus3, 2);
         Assertions.assertEquals(integerPlus4, 2);
-        Assertions.assertTrue(integerPlus5 == 2.2);
-        Assertions.assertEquals(integerPlus6, 2.2);
+        Assertions.assertTrue(integerPlus5.compareTo(2.2f) == 0);
+        Assertions.assertTrue(integerPlus6.compareTo(2.2d) == 0);
+        Assertions.assertTrue(integerPlus7.compareTo(0.2f) > 0);
+        Assertions.assertTrue(integerPlus8.compareTo(0.2d) > 0);
+        Assertions.assertEquals(integerPlus9, 2);
+        Assertions.assertEquals(integerPlus10, 2);
+        Assertions.assertEquals(integerPlus11, 2);
+    }
+
+    @Test
+    public void testMinus() throws SQLSyntaxException {
+        ExpressionContext mysql = new DefaultExpressionContext(DatabaseType.MySQL);
+        Expression numberPlus = $(1) - $(1);
+        Expression numberPlus2 = $(1) - ($(1) + $(1));
+        Expression numberPlus3 = $(1) - 1;
+        Expression numberPlus4 = $(1) - 1L;
+        Expression numberPlus5 = $(1) - (short)1;
+        Expression numberPlus6 = $(1) - (byte)1;
+        Expression numberPlus7 = $(1) - 1.2f;
+        Expression numberPlus8 = $(1) - 1.2d;
+
+        int integerPlus = 1 - 1;
+        long integerPlus2 = 1 - 1L;
+        int integerPlus3 = 1 - (short)1;
+        int integerPlus4 = 1 - (byte)1;
+        Float integerPlus5 = 1.2f - 1.2f;
+        Double integerPlus6 = 1.2d - 1.2d;
+        Float integerPlus7 = 1.2f - 1;
+        Double integerPlus8 = 1.2d - 1;
+        long integerPlus9 = 1 - 1L;
+        int integerPlus10 = 1 - (short)1;
+        int integerPlus11 = 1 - (byte)1;
+
+        Assertions.assertEquals(numberPlus.toSql(mysql), "((1 - 1))");
+        Assertions.assertEquals(numberPlus2.toSql(mysql), "((1 - ((1 + 1))))");
+        Assertions.assertEquals(numberPlus3.toSql(mysql), "((1 - 1))");
+        Assertions.assertEquals(numberPlus4.toSql(mysql), "((1 - 1))");
+        Assertions.assertEquals(numberPlus5.toSql(mysql), "((1 - 1))");
+        Assertions.assertEquals(numberPlus6.toSql(mysql), "((1 - 1))");
+        Assertions.assertEquals(numberPlus7.toSql(mysql), "((1 - 1.2))");
+        Assertions.assertEquals(numberPlus8.toSql(mysql), "((1 - 1.2))");
+
+        Assertions.assertEquals(integerPlus, 0);
+        Assertions.assertEquals(integerPlus2, 0);
+        Assertions.assertEquals(integerPlus3, 0);
+        Assertions.assertEquals(integerPlus4, 0);
+        Assertions.assertTrue(integerPlus5.compareTo(0f) == 0);
+        Assertions.assertTrue(integerPlus6.compareTo(0d) == 0);
+        Assertions.assertTrue(integerPlus7.compareTo(0.2f) > 0);
+        Assertions.assertTrue(integerPlus8.compareTo(0.2d) < 0);
+        Assertions.assertEquals(integerPlus9, 0);
+        Assertions.assertEquals(integerPlus10, 0);
+        Assertions.assertEquals(integerPlus11, 0);
+    }
+
+    @Test
+    public void testTimes() throws SQLSyntaxException {
+        ExpressionContext mysql = new DefaultExpressionContext(DatabaseType.MySQL);
+        Expression numberPlus = $(1) * $(1);
+        Expression numberPlus2 = $(1) * ($(1) + $(1));
+        Expression numberPlus3 = $(1) * 1;
+        Expression numberPlus4 = $(1) * 1L;
+        Expression numberPlus5 = $(1) * (short)1;
+        Expression numberPlus6 = $(1) * (byte)1;
+        Expression numberPlus7 = $(1) * 1.2f;
+        Expression numberPlus8 = $(1) * 1.2d;
+
+        int integerPlus = 1 * 1;
+        long integerPlus2 = 1 * 1L;
+        int integerPlus3 = 1 * (short)1;
+        int integerPlus4 = 1 * (byte)1;
+        Float integerPlus5 = 1.2f * 1f;
+        Double integerPlus6 = 1.2 * 1d;
+        Float integerPlus7 = 1.2f * 1;
+        Double integerPlus8 = 1.2d * 1;
+        long integerPlus9 = 1 * 1L;
+        int integerPlus10 = 1 * (short)1;
+        int integerPlus11 = 1 * (byte)1;
+
+        Assertions.assertEquals(numberPlus.toSql(mysql), "((1 * 1))");
+        Assertions.assertEquals(numberPlus2.toSql(mysql), "((1 * ((1 + 1))))");
+        Assertions.assertEquals(numberPlus3.toSql(mysql), "((1 * 1))");
+        Assertions.assertEquals(numberPlus4.toSql(mysql), "((1 * 1))");
+        Assertions.assertEquals(numberPlus5.toSql(mysql), "((1 * 1))");
+        Assertions.assertEquals(numberPlus6.toSql(mysql), "((1 * 1))");
+        Assertions.assertEquals(numberPlus7.toSql(mysql), "((1 * 1.2))");
+        Assertions.assertEquals(numberPlus8.toSql(mysql), "((1 * 1.2))");
+
+        Assertions.assertEquals(integerPlus, 1);
+        Assertions.assertEquals(integerPlus2, 1);
+        Assertions.assertEquals(integerPlus3, 1);
+        Assertions.assertEquals(integerPlus4, 1);
+        Assertions.assertTrue(integerPlus5 == 1.2f);
+        Assertions.assertTrue(integerPlus6 == 1.2d);
+        Assertions.assertTrue(integerPlus7 == 1.2f);
+        Assertions.assertTrue(integerPlus8 == 1.2d);
+        Assertions.assertEquals(integerPlus9, 1);
+        Assertions.assertEquals(integerPlus10, 1);
+        Assertions.assertEquals(integerPlus11, 1);
+    }
+
+    @Test
+    public void testDiv() throws SQLSyntaxException {
+        ExpressionContext mysql = new DefaultExpressionContext(DatabaseType.MySQL);
+        Expression numberPlus = $(1) / $(1);
+        Expression numberPlus2 = $(1) / ($(1) + $(1));
+        Expression numberPlus3 = $(1) / 1;
+        Expression numberPlus4 = $(1) / 1L;
+        Expression numberPlus5 = $(1) / (short)1;
+        Expression numberPlus6 = $(1) / (byte)1;
+        Expression numberPlus7 = $(1) / 1.2f;
+        Expression numberPlus8 = $(1) / 1.2d;
+
+        int integerPlus = 1 / 1;
+        long integerPlus2 = 1 / 1L;
+        int integerPlus3 = 1 / (short)1;
+        int integerPlus4 = 1 / (byte)1;
+        Float integerPlus5 = 1.2f / 1f;
+        Double integerPlus6 = 1.2 / 1d;
+        Float integerPlus7 = 1.2f / 1;
+        Double integerPlus8 = 1.2d / 1;
+        long integerPlus9 = 1 / 1L;
+        int integerPlus10 = 1 / (short)1;
+        int integerPlus11 = 1 / (byte)1;
+
+        Assertions.assertEquals(numberPlus.toSql(mysql), "((1 / 1))");
+        Assertions.assertEquals(numberPlus2.toSql(mysql), "((1 / ((1 + 1))))");
+        Assertions.assertEquals(numberPlus3.toSql(mysql), "((1 / 1))");
+        Assertions.assertEquals(numberPlus4.toSql(mysql), "((1 / 1))");
+        Assertions.assertEquals(numberPlus5.toSql(mysql), "((1 / 1))");
+        Assertions.assertEquals(numberPlus6.toSql(mysql), "((1 / 1))");
+        Assertions.assertEquals(numberPlus7.toSql(mysql), "((1 / 1.2))");
+        Assertions.assertEquals(numberPlus8.toSql(mysql), "((1 / 1.2))");
+
+        Assertions.assertEquals(integerPlus, 1);
+        Assertions.assertEquals(integerPlus2, 1);
+        Assertions.assertEquals(integerPlus3, 1);
+        Assertions.assertEquals(integerPlus4, 1);
+        Assertions.assertTrue(integerPlus5 == 1.2f);
+        Assertions.assertTrue(integerPlus6 == 1.2d);
+        Assertions.assertTrue(integerPlus7 == 1.2f);
+        Assertions.assertTrue(integerPlus8 == 1.2d);
+        Assertions.assertEquals(integerPlus9, 1);
+        Assertions.assertEquals(integerPlus10, 1);
+        Assertions.assertEquals(integerPlus11, 1);
     }
 }
