38a553bce4aadd15d6cf9f850c07ab337886edc4
Braisdom <yonghe.wang@joowing.com>
2020-11-16 13:46:15 +0800

    Add full sqlite example

---
+++ examples/sqlite/src/main/java/com/github/braisdom/objsql/example/SQLFile.java
@@ -0,0 +1,40 @@
+package com.github.braisdom.objsql.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SQLFile {
+
+    private List<String> sqls = new ArrayList<>();
+
+    public SQLFile(String classpathFile) throws IOException {
+        loadSqls(classpathFile);
+    }
+
+    private void loadSqls(String classpathFile) throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream(classpathFile);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } finally {
+            inputStream.close();
+            bufferedReader.close();
+        }
+        String[] rawSqls = stringBuilder.toString().split(";");
+        sqls.addAll(Arrays.asList(rawSqls));
+    }
+
+    public List<String> getSqls() {
+        return sqls;
+    }
+}
---
+++ examples/sqlite/src/main/resources/sqlite.sql
@@ -0,0 +1,22 @@
+drop table if exists members;
+drop table if exists orders;
+drop table if exists order_lines;
+
+create table members (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    no TEXT, name TEXT,
+    gender INTEGER,
+    mobile TEXT,
+    extended_attributes TEXT,
+    registered_at TEXT,
+    updated_at TEXT);
+
+create table orders (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    no TEXT, member_id INTEGER,
+    amount REAL,
+    quantity REAL,
+    sales_at TEXT);
+
+create table order_lines (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_no TEXT,
+    amount REAL,
+    quantity REAL);
