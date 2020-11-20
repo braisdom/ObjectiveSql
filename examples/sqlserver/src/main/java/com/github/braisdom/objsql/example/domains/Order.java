aec13ed17426d8f253dbb8715247a6903ede8e33
zxm <zxm-2018@qq.com>
2020-11-20 10:31:43 +0800

    Add mssql example

---
+++ examples/sqlserver/src/main/java/com/github/braisdom/objsql/example/domains/Order.java
@@ -0,0 +1,36 @@
+package com.github.braisdom.objsql.example.domains;
+
+import com.github.braisdom.objsql.annotations.Column;
+import com.github.braisdom.objsql.annotations.DomainModel;
+import com.github.braisdom.objsql.annotations.Relation;
+import com.github.braisdom.objsql.annotations.Transactional;
+import com.github.braisdom.objsql.relation.RelationType;
+import com.github.braisdom.objsql.transition.SqlDateTimeTransition;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+@DomainModel(primaryClass = BigDecimal.class)
+public class Order {
+    private String no;
+    private Long memberId;
+    private Float amount;
+    private Float quantity;
+
+    @Column(transition = SqlDateTimeTransition.class)
+    private Timestamp salesAt;
+
+    @Relation(relationType = RelationType.BELONGS_TO)
+    private Member member;
+
+    @Relation(relationType = RelationType.HAS_MANY)
+    private List<OrderLine> orderLines;
+
+    @Transactional
+    public static void makeOrder(Order order, OrderLine... orderLines) throws SQLException {
+        Order.create(order, false);
+        OrderLine.create(orderLines, false);
+    }
+}
