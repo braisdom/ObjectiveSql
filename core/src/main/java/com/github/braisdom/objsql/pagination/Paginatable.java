883e48203c0c3ef2120e8feb9d79c61740dbee6a
Braisdom <yonghe.wang@joowing.com>
2020-11-26 14:15:46 +0800

    Add pagination support

--- core/src/main/java/com/github/braisdom/objsql/pagination/Paginatable.java
+++ core/src/main/java/com/github/braisdom/objsql/pagination/Paginatable.java
@@ -18,6 +18,9 @@ package com.github.braisdom.objsql.pagination;
 
 import com.github.braisdom.objsql.DomainModelDescriptor;
 
+/**
+ * It describes a paged query object
+ */
 public interface Paginatable {
 
     String getQuerySQL(String databaseProductName);
